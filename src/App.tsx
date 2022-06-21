deee3cda266f2c05b86d9688462e39f2f22cf6e7
NaHua <nahua.qiao@gmail.com>
2022-06-21 15:02:08 +0800

    refactor🎨:  使用 React.lazy() 实现路由懒加载

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import './App.scss'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -10,20 +11,17 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
-import AppWrapper from './pages/AppWrapper'
-import Recommend from './pages/Recommend'
-import PlaylistDetail from './pages/PlaylistDetail'
-import NotFound from './pages/NotFound'
-import Test from './pages/Test'
+// import AppWrapper from './pages/AppWrapper'
+// import RecommendPlaylist from './pages/RecommendPlaylist'
+// import PlaylistDetail from './pages/PlaylistDetail'
+// import NotFound from './pages/NotFound'
+// import Test from './pages/Test'
 
-// import AppWrapper from '@/pages/AppWrapper'
-
-// import PlaylistDetail from '@/pages/PlaylistDetail'
-// import Test from '@/pages/Test'
-// import NotFound from '@/pages/NotFound'
-
-// import Recommend from '@/pages/Recommend'
-// const Recommend = React.lazy(() => import('@/pages/Recommend'))
+const AppWrapper = React.lazy(() => import('./pages/AppWrapper'))
+const RecommendPlaylist = React.lazy(() => import('./pages/RecommendPlaylist'))
+const PlaylistDetail = React.lazy(() => import('./pages/PlaylistDetail'))
+const NotFound = React.lazy(() => import('./pages/NotFound'))
+const Test = React.lazy(() => import('./pages/Test'))
 
 const App = () => {
   return (
@@ -32,8 +30,8 @@ const App = () => {
       <Router>
         <Routes>
           <Route path='/' element={<AppWrapper />}>
-            <Route path='/' element={<Recommend />} />
-            <Route path='/recommend' element={<Recommend />} />
+            <Route path='/' element={<RecommendPlaylist />} />
+            <Route path='/recommend' element={<RecommendPlaylist />} />
             <Route path='/playlist/:playlistId' element={<PlaylistDetail />} />
 
             <Route path='/test' element={<Test />} />
